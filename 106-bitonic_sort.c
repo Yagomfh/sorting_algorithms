cfa9547b24eaaa52fa0483caf530bb877ebb2b56
yagomfh <yagomfh@gmail.com>
2021-02-01 13:59:08 +0000

    feat: 1st commit

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,5 +1,36 @@
 #include "sort.h"
 
+/**
+ * print_sublist - prints sub_list
+ * @array: the array source
+ * @lo: low index
+ * @hi: high index
+ * Return: void
+ */
+
+void print_sublist(int *array, int lo, int hi)
+{
+	int i;
+
+	for (i = lo; i <= hi; i++)
+	{
+		printf("%d", array[i]);
+		if (i != hi)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+
+/**
+  * comp_swap - compare swap
+  * @a: array to use
+  * @i: 1st elem
+  * @j: 2nd elem
+  * @dir: direction to swap
+  * Return: void
+  */
+
 void comp_swap(int *a, int i, int j, int dir)
 {
 	int tmp;
@@ -12,7 +43,17 @@ void comp_swap(int *a, int i, int j, int dir)
 	}
 }
 
-void bitonic_merge(int *a, int lo, int cnt, int dir)
+/**
+  * bitonic_merge - bitonic merge
+  * @a: array to use
+  * @lo: lower index
+  * @cnt: count
+  * @dir: direction
+  * @ts: original size of array
+  * Return: void
+  */
+
+void bitonic_merge(int *a, int lo, int cnt, int dir, int ts)
 {
 	int i, k;
 
@@ -21,24 +62,52 @@ void bitonic_merge(int *a, int lo, int cnt, int dir)
 		k = cnt / 2;
 		for (i = lo; i < lo + k; i++)
 			comp_swap(a, i, i + k, dir);
-		bitonic_merge(a, lo, k, dir);
-		bitonic_merge(a, lo + k, k, dir);
+		bitonic_merge(a, lo, k, dir, ts);
+		bitonic_merge(a, lo + k, k, dir, ts);
 	}
 }
 
-void bitonic(int *a, int lo, int cnt, int dir)
+/**
+  * bitonic - bitonic sorting algorithm
+  * @a: array to sort
+  * @lo: lower index
+  * @cnt: count
+  * @dir: direction
+  * @ts: original size of array
+  * Return: void
+  */
+
+void bitonic(int *a, int lo, int cnt, int dir, int ts)
 {
 	int k;
 
 	if (cnt > 1)
 	{
+		printf("Merging [%d/%d] ", cnt, ts);
+		if (dir == 0)
+			printf("(DOWN):\n");
+		else
+			printf("(UP):\n");
+		print_sublist(a, lo, lo + cnt - 1);
 		k = cnt / 2;
-		bitonic(a, lo, k, 1);
-		bitonic(a, lo + k, k , 0);
-		bitonic_merge(a, lo, cnt, dir);
-		printf("lo = %d | cnt = %d | dir = %d\n", lo, cnt, dir);
+		bitonic(a, lo, k, 1, ts);
+		bitonic(a, lo + k, k, 0, ts);
+		bitonic_merge(a, lo, cnt, dir, ts);
+		printf("Result [%d/%d] ", cnt, ts);
+		if (dir == 0)
+			printf("(DOWN):\n");
+		else
+			printf("(UP):\n");
+		print_sublist(a, lo, lo + cnt - 1);
 	}
 }
+
+/**
+  * bitonic_sort - bitonic sorting algo entry point
+  * @array: array to sort
+  * @size: size of array
+  * Return: void
+  */
 
 void bitonic_sort(int *array, size_t size)
 {
@@ -47,5 +116,5 @@ void bitonic_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	bitonic(array, 0, s, up);
+	bitonic(array, 0, s, up, s);
 }
