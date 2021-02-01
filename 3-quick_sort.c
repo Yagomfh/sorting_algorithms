fd83ba96c6e531ec9cd89c63dc3993517635c13f
yagomfh <yagomfh@gmail.com>
2021-02-01 09:30:36 +0000

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -83,7 +83,7 @@ void qs(int *array, int lo, int hi, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 	qs(array, 0, size - 1, size);
 }
